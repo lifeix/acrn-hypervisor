1b2c4b46b8ef631d122d7cf1004ef6c852738f29
Fei Li <fei1.li@intel.com>
2021-07-29 10:30:51 +0800

    hv: remove the constraint "MMU and EPT must both support large page or not"
    There're some virtual platform which doesn't meet this constraint. So remove
    this constraint.
    
    Tracked-On: #6329
    Signed-off-by: Fei Li <fei1.li@intel.com>

--- hypervisor/arch/x86/mmu.c
+++ hypervisor/arch/x86/mmu.c
@@ -72,7 +72,7 @@ static inline bool ppt_large_page_support(enum _page_table_level level, __unused
 	if (level == IA32E_PD) {
 		support = true;
 	} else if (level == IA32E_PDPT) {
-		support = pcpu_has_vmx_ept_vpid_cap(VMX_EPT_1GB_PAGE);
+		support = pcpu_has_cap(X86_FEATURE_PAGE1GB);
 	} else {
 		support = false;
 	}
