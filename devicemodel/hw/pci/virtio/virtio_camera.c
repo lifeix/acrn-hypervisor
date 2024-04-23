420206c4305bcc28694ed7080c3aa28c0fd490e1
Chenli Wei <chenli.wei@intel.com>
2024-04-23 11:26:54 +0800

    dm: adjusts the positions of some function and structure
    This patch adjusts the positions of some function and structure
    definitions, so that they use the same set of definitions as the
    camera daemon module.
    
    Tracked-On: AUTOMOTIVE-1330
    Signed-off-by: Chenli Wei <chenli.wei@intel.com>

--- devicemodel/hw/pci/virtio/virtio_camera.c
+++ devicemodel/hw/pci/virtio/virtio_camera.c
@@ -26,8 +26,8 @@
 #include "virtio_camera.h"
 #include "virtio_kernel.h"
 #include "vmmapi.h"
-#include "vICamera.h"
 #include "camera_config_interface.h"
+#include "camera_utils.h"
 
 #define VIRTIO_CAMERA_MAXSEGS 256
 #define SHARE_CAMERA
@@ -58,53 +58,6 @@ struct camera_config_ops {
 
 static void *g_camera_config_handle = NULL;
 static struct camera_config_ops g_camera_config_ops = {0};
-
-#define GET_SYMBOL(handle, p, symbol)                                                                                  \
-	(p) = (typeof(p))dlsym((handle), (symbol));                                                                    \
-	if ((p) == NULL) {                                                                                             \
-		pr_info("Failed to find function in %s %s\n", library_name, dlerror());                                \
-	}                                                                                                              \
-	pr_info("find %s\n", (symbol));
-
-static int fill_hal_ops(char *library_name, void **handle, struct camera_ops *hal_ops)
-{
-	void *hal_handle = dlopen(library_name, RTLD_LAZY);
-
-	if (hal_handle == NULL) {
-		pr_info("Failed to open %s %s\n", library_name, dlerror());
-	} else {
-		GET_SYMBOL(hal_handle, hal_ops->get_camera_info, "vcamera_get_camera_info");
-		GET_SYMBOL(hal_handle, hal_ops->hal_init, "vcamera_hal_init");
-		GET_SYMBOL(hal_handle, hal_ops->hal_deinit, "vcamera_hal_deinit");
-		GET_SYMBOL(hal_handle, hal_ops->open, "vcamera_device_open");
-		GET_SYMBOL(hal_handle, hal_ops->close, "vcamera_device_close");
-		GET_SYMBOL(hal_handle, hal_ops->config_sensor_input, "vcamera_device_config_sensor_input");
-		GET_SYMBOL(hal_handle, hal_ops->config_streams, "vcamera_device_config_streams");
-		GET_SYMBOL(hal_handle, hal_ops->start_stream, "vcamera_device_start");
-		GET_SYMBOL(hal_handle, hal_ops->stop_stream, "vcamera_device_stop");
-		GET_SYMBOL(hal_handle, hal_ops->allocate_memory, "vcamera_device_allocate_memory");
-		GET_SYMBOL(hal_handle, hal_ops->get_frame_size, "vcamera_get_frame_size");
-		GET_SYMBOL(hal_handle, hal_ops->stream_qbuf, "vcamera_stream_qbuf");
-		GET_SYMBOL(hal_handle, hal_ops->stream_dqbuf, "vcamera_stream_dqbuf");
-		GET_SYMBOL(hal_handle, hal_ops->set_parameters, "vcamera_set_parameters");
-		GET_SYMBOL(hal_handle, hal_ops->get_parameters, "vcamera_get_parameters");
-		GET_SYMBOL(hal_handle, hal_ops->get_formats_number, "vcamera_get_formats_number");
-		GET_SYMBOL(hal_handle, hal_ops->get_formats, "vcamera_get_formats");
-		GET_SYMBOL(hal_handle, hal_ops->set_client_name, "vcamera_set_client_name");
-		*handle = hal_handle;
-		return 0;
-	}
-	return -1;
-}
-
-void close_hal_handle(void *hal_handle, struct camera_ops *hal_ops)
-{
-	memset(&hal_ops, 0, sizeof(hal_ops));
-	if (hal_handle) {
-		dlclose(hal_handle);
-		hal_handle = NULL;
-	}
-}
 
 static int fill_config_ops(char *library_name, void **handle, struct camera_config_ops *camera_config_ops)
 {
@@ -1186,7 +1139,7 @@ static int virtio_camera_init(struct vmctx *ctx, struct pci_vdev *dev, char *opt
 	struct virtio_camera *vcamera;
 	pthread_mutexattr_t attr;
 	int i;
-	char *opt;
+	char *opt = NULL;
 	int32_t ret = -1;
 
 	pr_err("vcamera init: vmctx info, vm vmid is %d, vm name is %s\n", ctx->vmid, ctx->name);
--- devicemodel/hw/pci/virtio/virtio_camera.h
+++ devicemodel/hw/pci/virtio/virtio_camera.h
@@ -10,6 +10,7 @@
 #define __VIRTIO_CAMERA_H__
 
 #include "vcamhal_type.h"
+#include "vICamera.h"
 
 #define MAX_VIRTIO_CAMERA_NUMQ 8
 // set the specific ID for frontend to recongnise
@@ -164,41 +165,6 @@ struct capture_buffer {
 	struct virtio_camera_request *response;
 	char reserve[2];
 };
-
-struct camera_ops {
-	int (*get_camera_info)(int camera_id, void *data);
-
-	int (*open)(int camera_id);
-	void (*close)(int camera_id);
-
-	int (*allocate_memory)(int camera_id, camera_buffer_t *buffer);
-
-	int (*config_streams)(int camera_id, stream_config_t *stream_list);
-	int (*start_stream)(int camera_id);
-	int (*stop_stream)(int camera_id);
-
-	int (*stream_qbuf)(int camera_id, camera_buffer_t **buffer, int num_buffers, void *settings);
-	int (*stream_dqbuf)(int camera_id, int stream_id, camera_buffer_t **buffer, void *settings);
-
-	int (*hal_init)();
-	int (*hal_deinit)();
-	int (*config_sensor_input)(int camera_id, const stream_t *inputConfig);
-	int (*get_frame_size)(int camera_id, int format, int width, int height, int field, int *bpp);
-	int (*set_exposure)(int camera_id, int date);
-	int (*set_parameters)(int camera_id, void *data);
-	int (*get_parameters)(int camera_id, void *data, int64_t sequence);
-	int (*req_bufs)(int camera_id);
-	int (*get_formats_number)(int camera_id);
-	int (*get_formats)(int camera_id, stream_t* p, int* streams_number);
-	int (*set_client_name)(char *name, int size);
-};
-
-typedef enum _type {
-	V4L2_INTERFACE = 0,
-	HAL_INTERFACE = 1,
-	PROXY_INTERFACE = 2,
-	SUPER_FRAME_INTERFACE = 3,
-} interface_type;
 
 struct camera_dev {
 	int id;
