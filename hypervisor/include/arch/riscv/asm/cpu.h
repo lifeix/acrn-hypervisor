d9a247be1c7aabb0a463920fb9fd2c5552dc70b8
Fei Li <fei1.li@intel.com>
2025-09-23 23:21:04 +0800

    smap: risc-v should implement enable and disable APIs
    And should align the name with other ARCHs.
    
    Signed-off-by: Fei Li <fei1.li@intel.com>

--- hypervisor/include/arch/riscv/asm/cpu.h
+++ hypervisor/include/arch/riscv/asm/cpu.h
@@ -80,4 +80,8 @@ void wait_sync_change(volatile const uint64_t *sync, uint64_t wake_sync);
 void init_percpu_hart_id(uint32_t bsp_hart_id);
 uint16_t get_pcpu_id_from_hart_id(uint32_t hart_id);
 
+/* FIXME: If RISC-V support sbuf, it should implement SMAP enable/disable APIs */
+static inline void stac(void) {}
+static inline void clac(void) {}
+
 #endif /* RISCV_CPU_H */
